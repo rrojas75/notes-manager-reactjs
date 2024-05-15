5f6bc4d2c36d2dc3c6d89380f5f766362fb2a5fa
nick <romnick.rojas75@gmail.com>
2024-05-16 07:17:08 +0800

    search filter

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -1,12 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import s from "./style.module.css";
 import { TextCard } from "components/TextCard/TextCard";
 import { useNavigate } from "react-router-dom";
 import { NoteAPI } from "api/note-api";
 import { deleteNote } from "store/notes/note-slice";
 
-export function NoteList(props) {
-  const noteList = useSelector((store) => store.noteSlice.noteList);
+export function NoteList({ noteList }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
