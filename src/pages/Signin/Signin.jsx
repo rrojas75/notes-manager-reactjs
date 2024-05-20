3999b1266311015cf0c1eb4b7a27716a9648b90b
nick <romnick.rojas75@gmail.com>
2024-05-21 07:27:35 +0800

    firebase setup

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -3,17 +3,31 @@ import s from "./style.module.css";
 import { Link } from "react-router-dom";
 import { Input } from "components/Input/Input";
 import { AuthLayout } from "layouts/AuthLayout/AuthLayout";
+import { useState } from "react";
 export function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = (e) => {
+    console.log("submitted ", email, password);
+    e.preventDefault();
+  };
   const form = (
     <div className={s.formContainer}>
       <h2 className={s.title}>
         Signin <br />
         to access your team notes
       </h2>
-      <form className={s.formGroup}>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" type="password" />
-        <ButtonPrimary className={s.button}>Sign in!</ButtonPrimary>
+      <form onSubmit={submit} className={s.formGroup}>
+        <Input placeholder="Email" onTextChange={setEmail} />
+        <Input
+          placeholder="Password"
+          type="password"
+          onTextChange={setPassword}
+        />
+        <ButtonPrimary type="submit" className={s.button}>
+          Sign in!
+        </ButtonPrimary>
         <span>
           Don't have an account yet ? <Link to="/signup">Signup</Link>
         </span>
